bd5fc34681a3aa7505497b577d78a1f4ec62bf37
Marcel Wysocki <maci.stgn@gmail.com>
2019-07-03 11:56:58 -0700

    remove old version before running the new

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+flatpak remove -y org.yuzu_emu.yuzu
 flatpak --user remote-add --no-gpg-verify --if-not-exists yuzu-repo yuzu-repo
-flatpak --user install yuzu-repo org.yuzu_emu.yuzu
+flatpak --user -y install yuzu-repo org.yuzu_emu.yuzu
 flatpak run org.yuzu_emu.yuzu
