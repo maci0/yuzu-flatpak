ed6830caaeca08a838505c36f5aec328ac619714
Marcel Wysocki <maci.stgn@gmail.com>
2019-07-03 12:37:53 -0700

    revert back to x11 for now

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+rm -Rf yuzu-repo
+
 pushd org.yuzu-emu.yuzu
-rm -Rf build ../yuzu-repo
+
+rm -Rf build
 flatpak-builder --ccache --repo=../yuzu-repo build org.yuzu_emu.yuzu.json 
 
 popd
--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-flatpak remove -y org.yuzu_emu.yuzu
+# remove old
+flatpak remove -y org.yuzu_emu.yuzu || true
+flatpak --user remote-delete yuzu-repo || true
+
+# add new
 flatpak --user remote-add --no-gpg-verify --if-not-exists yuzu-repo yuzu-repo
 flatpak --user -y install yuzu-repo org.yuzu_emu.yuzu
+
+# run
 flatpak run org.yuzu_emu.yuzu
